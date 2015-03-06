3b14ed7d9ec092baaf10c9f81955dda28508db35
Robert Luce <luce@math.tu-berlin.de>
2015-03-06 18:00:43 +0100

    Add test for correct image extent

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,7 @@
 import unittest
 from phaseplot import phase_portrait
 import matplotlib
+from matplotlib import pyplot as plt
 
 class TestBasics(unittest.TestCase):
     """A collection of basic tests with no particular theme"""
@@ -10,3 +11,19 @@ class TestBasics(unittest.TestCase):
         def somefun(z): return z*z + 1
         retval = phase_portrait(somefun)
         self.assertIsInstance(retval, matplotlib.image.AxesImage)
+
+
+    def test_extent(self):
+        """Test that the 'box' argument matches extent"""
+
+        # See also: issue #1
+
+        ai = phase_portrait(lambda(z) : z, box = [-1,1,-1,1])
+        # extent = (left, right, bottom, top)
+        extent = ai.get_extent()
+        self.assertEqual( extent, (-1, 1, -1, 1) )
+
+        ai = phase_portrait(lambda(z) : z, box = [-1,2,-3,4])
+        # extent = (left, right, bottom, top)
+        extent = ai.get_extent()
+        self.assertEqual( extent, (-1, 2, -3, 4) )
